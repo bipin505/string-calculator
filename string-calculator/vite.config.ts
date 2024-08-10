72a4740f6f3b1d49cb25dc9eb5ce0ce9fa887652
bipin505 <41504003+bipin505@users.noreply.github.com>
2024-08-10 14:26:08 +0530

    build fixes

--- string-calculator/vite.config.ts
+++ string-calculator/vite.config.ts
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom', // Ensure this is set to 'jsdom'
-    globals: true,
-    setupFiles: './src/setupTests.ts',
-  },
 });
